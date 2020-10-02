8c3f024647f38f4a43f669fa4981e25c6173012e
chlwhdtn03 <chlwhdtn03@naver.com>
2020-10-02 11:15:51 +0900

    회원가입 뒤로가기 버튼

--- app/src/main/java/com/chlwhdtn/grouping/RegisterActivity.java
+++ app/src/main/java/com/chlwhdtn/grouping/RegisterActivity.java
@@ -3,12 +3,21 @@ package com.chlwhdtn.grouping;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    ImageView backbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        backbtn = findViewById(R.id.reg_back);
+
+        backbtn.setOnClickListener(view -> {
+            finish();
+        });
     }
 }
