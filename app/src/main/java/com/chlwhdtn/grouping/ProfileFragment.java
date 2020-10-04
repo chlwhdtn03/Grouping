fafe223cde9e7eb9e5f438605bfdd273d9a38b56
chlwhdtn03 <chlwhdtn03@naver.com>
2020-10-04 14:17:42 +0900

    프로필 정보

--- app/src/main/java/com/chlwhdtn/grouping/ProfileFragment.java
+++ app/src/main/java/com/chlwhdtn/grouping/ProfileFragment.java
@@ -4,18 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.chlwhdtn.grouping.Util.UserManager;
+
 public class ProfileFragment extends Fragment {
+
+    TextView username, id;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View view =  inflater.inflate(R.layout.fragment_profile, container, false);
+
+        username = view.findViewById(R.id.profile_username);
+        id = view.findViewById(R.id.profile_id);
+
+        username.setText(UserManager.getMy().getUsername());
+        id.setText(UserManager.getMy().getId());
+
+        return view;
     }
 }
