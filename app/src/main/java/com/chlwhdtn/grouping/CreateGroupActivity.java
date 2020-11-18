6c615b3144a2c35635368ea47cb648e445926dcd
chlwhdtn03 <chlwhdtn03@naver.com>
2020-11-18 22:36:29 +0900

    여러가지..^^

--- app/src/main/java/com/chlwhdtn/grouping/CreateGroupActivity.java
+++ app/src/main/java/com/chlwhdtn/grouping/CreateGroupActivity.java
@@ -7,6 +7,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.chlwhdtn.grouping.Data.CommonResult;
+import com.chlwhdtn.grouping.Data.LoginObject;
+import com.chlwhdtn.grouping.Retrofit.GroupingRetrofit;
+import com.chlwhdtn.grouping.Retrofit.GroupingService;
+import com.chlwhdtn.grouping.Util.MessageBox;
+import com.chlwhdtn.grouping.Util.MessageType;
+import com.chlwhdtn.grouping.Util.UserManager;
+
+import retrofit2.Call;
+
 public class CreateGroupActivity extends AppCompatActivity {
 
     EditText joingroup_edit, creategroup_edit;
@@ -23,6 +33,14 @@ public class CreateGroupActivity extends AppCompatActivity {
 
         creategroup_btn = findViewById(R.id.creategroup_btn);
         creategroup_edit = findViewById(R.id.creategroup_name);
+
+        joingroup_btn.setOnClickListener(item -> {
+            if(joingroup_edit.getText().toString().isEmpty()) {
+                MessageBox.show(item, "그룹 코드를 입력하세요", MessageType.ERROR);
+            }
+            GroupingService retrofit = GroupingRetrofit.getInstance(getBaseContext()).getGroupingService();
+            Call<CommonResult> response = retrofit.joinGroup("Bearer " + UserManager.getAccount(this).getAccessToken(), joingroup_edit.getText().toString());
+        });
 
         backbtn = findViewById(R.id.group_back);
 
---
+++ app/src/main/java/com/chlwhdtn/grouping/adapter/ScheduleAdapter.java
@@ -0,0 +1,58 @@
+package com.chlwhdtn.grouping.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.chlwhdtn.grouping.Data.Schedule;
+import com.chlwhdtn.grouping.R;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class ScheduleAdapter extends RecyclerView.Adapter<ScheduleAdapter.ViewHolder> {
+
+    public ArrayList<Schedule> list;
+
+    public ScheduleAdapter(ArrayList<Schedule> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_schedule, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.date.setText(list.get(position).getDate());
+        holder.group.setText(list.get(position).getOwner());
+        holder.title.setText(list.get(0).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView title, date, group;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.item_schedule_title);
+            group = itemView.findViewById(R.id.item_group_title);
+            date = itemView.findViewById(R.id.item_schedule_date);
+        }
+
+
+    }
+
+}
